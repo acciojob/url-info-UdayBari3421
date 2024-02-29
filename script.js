d53ad15d62b41473dfe349d2bce1f7b93903c1e2
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-02-29 21:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 
-const currentURL = window.location.href;
+const currentURL = window.location.origin;
 const n = currentURL.length;
 alert(`The length of the URL is: ${n}`);
